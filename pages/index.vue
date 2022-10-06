f621322b785f03eba2798254a6347e07abc61844
Gerasjeka1985 <gekgrom@mail.ru>
2022-10-06 18:04:33 +0300

    background

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <h1>Hello world!</h1>
-  <h2>Darling Ila its me Jeka ))))</h2>
+  <div class="main">
+    <h1>Hello world!</h1>
+    <h2>Darling Ila its me Jeka ))))</h2>
 
-  <p>click me )))</p>
-  <button style="width: 100px;height: 50px;border-radius: 5px" @click="handler">Click</button>
-  <div v-if="a">
-    <h2>Hi my love ))))</h2>
+    <p>click me )))</p>
+    <button style="width: 100px;height: 50px;border-radius: 5px" @click="handler">Click</button>
+    <div v-if="a">
+      <h2>Hi my love ))))</h2>
+    </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
@@ -17,3 +20,19 @@ import {ref} from 'vue';
   }
 
 </script>
+
+<style scoped>
+.main{
+  color: aliceblue;
+  width: 100%;
+  height: 100vh;
+  background-image: url("@/assets/images/fdfe5baa-657f-44da-84cb-7f3e4ace13a4_1.jpg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
