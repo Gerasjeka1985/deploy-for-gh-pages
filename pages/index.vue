173496f4519926c328d238c77d2079ad56bcdb48
Gerasjeka1985 <gekgrom@mail.ru>
2022-10-06 17:39:46 +0300

    click

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
   <h1>Hello world!</h1>
   <h2>Darling Ila its me Jeka ))))</h2>
+
+  <p>click me )))</p>
+  <button style="width: 100px;height: 50px;border-radius: 5px" @click="handler">Click</button>
 </template>
+
+<script setup lang="ts">
+  const handler = () => {
+    alert('kiss you darling ))))')
+  }
+
+</script>
