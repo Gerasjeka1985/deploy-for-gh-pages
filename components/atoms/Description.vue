1b728b53470ada1324fc30508f5dfc632f17f678
Gerasjeka1985 <gekgrom@mail.ru>
2022-10-09 22:59:27 +0300

    descr add

---
+++ components/atoms/Description.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="description">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Alias atque corporis cupiditate dolor eos itaque magnam molestias
+    odit quam, soluta, tempore veritatis voluptates. Architecto eveniet
+    explicabo impedit quaerat sed. Eius et facere hic iure maiores nemo
+    pariatur perferendis rerum vitae voluptates? Earum illum incidunt quae reiciendis,
+    similique tenetur vitae! Alias debitis facere fuga, itaque neque numquam ratione suscipit
+    veritatis voluptates. Facilis ipsa laborum molestias sit tempore tenetur, veniam. Architecto assumenda,
+    aut dolore earum eligendi fugit magnam officia quibusdam totam vero? A accusamus aut eum,
+    facere id incidunt iure labore magni molestias mollitia, nam nulla officia porro sunt tempora,
+    velit voluptas voluptatibus!
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_global.scss';
+  .description{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #090979;
+  }
+
+@include breakpoint(large) {
+  .description{
+    width: 600px;
+    height: 400px;
+    margin: auto;
+  }
+}
+</style>
--- components/organism/Content.vue
+++ components/organism/Content.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
   import CustomBanner from "~/components/atoms/CustomBanner.vue";
   import Carusel from "~/components/atoms/Carusel.vue";
+  import Description from "~/components/atoms/Description.vue";
 </script>
 
 <template>
   <div class="content">
     <custom-banner />
     <Carusel />
+    <Description />
   </div>
 </template>
 
